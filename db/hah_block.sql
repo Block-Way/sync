01021799a2d20d818adc129b08bc5fbcb172ae19
mamchain <mamchain@protonmail.com>
2022-03-21 15:41:45 +0800

    mod block tx

--- db/hah_block.sql
+++ db/hah_block.sql
@@ -16,41 +16,31 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `Block`
+-- Table structure for table `block`
 --
 
-DROP TABLE IF EXISTS `Block`;
+DROP TABLE IF EXISTS `block`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `Block` (
+CREATE TABLE `block` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `hash` varchar(64) DEFAULT NULL COMMENT '区块hash(区块的ID)',
-  `fork_hash` varchar(64) DEFAULT NULL COMMENT '分支的hash',
-  `prev_hash` varchar(64) DEFAULT NULL COMMENT '上一区块的hash',
-  `time` bigint(20) DEFAULT NULL COMMENT '时间戳',
-  `height` int(11) DEFAULT NULL COMMENT '高度',
-  `type` varchar(16) DEFAULT NULL COMMENT '区块类型，pow,dpos',
-  `reward_address` varchar(64) DEFAULT NULL COMMENT '出块奖励地址',
-  `reward_money` decimal(20,10) DEFAULT NULL COMMENT '出块奖励金额',
-  `is_useful` bit(1) DEFAULT b'1' COMMENT '是否有效，为最长链的区块',
-  `bits` int(255) DEFAULT NULL COMMENT '难度',
-  `reward_state` bit(1) DEFAULT b'0' COMMENT 'DPOS出块收益计算状态',
-  PRIMARY KEY (`id`) USING BTREE,
+  `hash` varchar(64) CHARACTER SET utf8 DEFAULT NULL,
+  `prev_hash` varchar(64) CHARACTER SET utf8 DEFAULT NULL,
+  `time` bigint(20) DEFAULT NULL,
+  `height` int(11) DEFAULT NULL,
+  `reward_address` varchar(64) CHARACTER SET utf8 DEFAULT NULL,
+  `reward_money` decimal(20,8) DEFAULT NULL,
+  `bits` decimal(30,2) DEFAULT NULL,
+  `is_useful` bit(1) DEFAULT b'1',
+  `type` varchar(64) DEFAULT NULL COMMENT '区块类型',
+  `txs` bigint(20) DEFAULT NULL COMMENT '区块的交易数量',
+  PRIMARY KEY (`id`),
   KEY `hash` (`hash`) USING BTREE,
+  KEY `time` (`time`) USING BTREE,
   KEY `height` (`height`) USING BTREE,
-  KEY `reward_address` (`reward_address`) USING BTREE,
-  KEY `time` (`time`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+  KEY `reward_address` (`reward_address`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=6886 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `Block`
---
-
-LOCK TABLES `Block` WRITE;
-/*!40000 ALTER TABLE `Block` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Block` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -61,4 +51,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-03-21 14:55:14
+-- Dump completed on 2022-03-21 15:40:05
